6d286ac1619932770cc836d78d50b2bb3437d948
USA\lovep <Paul.Love@mkcorp.com>
2019-07-17 13:56:27 -0500

    first commit of simple vue page - 1st lesson

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+
+<template id="">
+  <div class="">
+    <h1>{{ title }}</h1>
+
+  </div>
+</template>
+<script type="text/javascript">
+  export default  {
+    name: "BookList",
+    data(){
+      return {
+        title: "All Books are here dude"
+      };
+    }
+  }
+</script>
+<style media="screen">
+  h1, h2 {
+    font-weight: normal;
+  }
+
+</style>
