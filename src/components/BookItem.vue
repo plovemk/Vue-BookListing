fd925c83ad4bf1c164066007360e6ea22d360947
USA\lovep <Paul.Love@mkcorp.com>
2019-07-23 13:34:37 -0500

    module 3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,18 @@
+<template id="">
+  <!-- <ul> -->
+    <!-- <li>{{ book.title }}: {{ book.author }}</li> -->
+    <li style="" v-for="book in books">{{ book.title}}: {{book.author}}</li>
+  <!-- </ul> -->
+</template>
+<script type="text/javascript">
+  export default {
+    name: 'BookItem',
+    props: [
+      'book'
+    ]
+  }
+</script>
+<style media="screen">
+
+
+</style>
